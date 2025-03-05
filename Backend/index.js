31cb32951dbb10230bdb3529b4e350519d828779
Aashish Khadka <aashishkhadka720@gmail.com>
2025-03-05 21:09:50 +0545

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,28 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
+
+
+
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000;
+const Mongo = process.env.Mongo;
+
+mongoose.connect(Mongo, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("MongoDB connection error:", error);
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World! f hdallo')
-})
+    res.send('Hello World!')
+})    
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
